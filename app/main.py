56380edc12da6b85ed69ab74bff579fc7e3a7a45
Aditya Suryawanshi <adityassuryawanshi25@gmail.com>
2026-07-26 21:27:28 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse
 
 app = FastAPI(
@@ -10,14 +11,18 @@ app = FastAPI(
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+templates = Jinja2Templates(directory="app/templates")
+
 
 @app.get("/")
-async def root():
-    return {
-        "name": "ZeroTrust One",
-        "status": "Running",
-        "version": "1.0.0"
-    }
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "title": "ZeroTrust One"
+        }
+    )
 
 
 @app.get("/health")
